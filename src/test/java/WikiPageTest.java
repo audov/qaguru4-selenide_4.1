ad68d97d1b69321f1d3d1eeae905679f4b7a6cbf
audov <audov@mail.ru>
2024-08-26 16:14:10 +0300

    initial commit

---
+++ src/test/java/WikiPageTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WikiPageTest {
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        baseUrl = "https://github.com/selenide/selenide";
+    }
+
+    @Test
+    void softAssertionsAndJUnit5ShouldBeAvailTest() {
+        open(baseUrl);
+        $("#wiki-tab").click();
+        $(".filter-bar #wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                    @Test
+                    void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                    """));
+    }
+}
